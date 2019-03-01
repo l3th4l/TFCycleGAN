13d220c9b27ca317e732f48e0779c2e5ee80ce7f
L3th4l <safalyaglobal@gmail.com>
2019-03-01 22:42:53 +0530

    sudoku

--- nets.py
+++ nets.py
@@ -1,49 +1,44 @@
 import tensorflow as tf 
-from tensorflow.contrib import layers
+from tensorflow import layers
 
-def conv2d(x, kernel_size, channels, strides = [1, 1], padding = 'SAME'):
-    return layers.conv2d(x, num_outputs = channels, kernel_size = kernel_size, stride = strides, padding = padding)
+def conv2d(x, kernel_size, channels, strides = [1, 1], padding = 'same', name = None):
+    return layers.conv2d(x, filters = channels, kernel_size = kernel_size, strides = strides, padding = padding, name = name)
 
-def tConv2d(x, kernel_size, channels, strides = [1, 1], padding = 'SAME'):
-    return layers.conv2d_transpose(x, num_outputs = channels, kernel_size = kernel_size, stride = strides, padding = padding)
+def tConv2d(x, kernel_size, channels, strides = [1, 1], padding = 'same', name = None):
+    return layers.conv2d_transpose(x, filters = channels, kernel_size = kernel_size, strides = strides, padding = padding, name = name)
 
-def res_block(x, kernel_size, channels):
-    r1 = conv2d(x, kernel_size, channels)
-    r2 = conv2d(r1, kernel_size, channels)
+def res_block(x, kernel_size, channels, name = 'res'):
+    r1 = conv2d(x, kernel_size, channels, name = name + '1')
+    r2 = conv2d(r1, kernel_size, channels, name = name + '2')
     return r1 + r2
 
-class Gen():
-
-    def __init__(self, x):
-        #96 x 96 x 3
-
+def gen(x, name = 'gen'):
+    with tf.variable_scope(name):
+        #96 x 96 x 3      
         #Encoder 
-        self.cnv1 = conv2d(x, [3, 3], 8, strides = [2, 2]) # --> 48 x 48 x 8
-        self.cnv2 = conv2d(self.cnv1, [3, 3], 16, strides = [2, 2]) # --> 24 x 24 x 16
-        self.cnv3 = conv2d(self.cnv2, [3, 3], 32, strides = [2, 2]) # --> 12 x 12 x 32
-        self.cnv4 = conv2d(self.cnv3, [3, 3], 64, strides = [2, 2]) # --> 6 x 6 x 64
-
+        cnv1 = conv2d(x, [3, 3], 8, strides = [2, 2], name = 'cnv1') # --> 48 x 48 x 8
+        cnv2 = conv2d(cnv1, [3, 3], 16, strides = [2, 2], name = 'cnv2') # --> 24 x 24 x 16
+        cnv3 = conv2d(cnv2, [3, 3], 32, strides = [2, 2], name = 'cnv3') # --> 12 x 12 x 32
+        cnv4 = conv2d(cnv3, [3, 3], 64, strides = [2, 2], name = 'cnv4') # --> 6 x 6 x 64
         #Decoder 
-        self.tcnv1 = tConv2d(self.cnv4, [3, 3], 32, strides = [2, 2]) # --> 12 x 12 x 32
-        self.tcnv2 = tConv2d(self.tcnv1 + self.cnv3, [3, 3], 16, strides = [2, 2]) # --> 24 x 24 x 16
-        self.tcnv3 = tConv2d(self.tcnv2 + self.cnv2, [3, 3], 8, strides = [2, 2]) # --> 48 x 48 x 8
-        self.tcnv4 = tConv2d(self.tcnv3 + self.cnv1, [3, 3], 3, strides = [2, 2]) # --> 96 x 96 x 3
-
+        tcnv1 = tConv2d(cnv4, [3, 3], 32, strides = [2, 2], name = 'tcnv1') # --> 12 x 12 x 32
+        tcnv2 = tConv2d(tcnv1 + cnv3, [3, 3], 16, strides = [2, 2], name = 'tcnv2') # --> 24 x 24 x 16
+        tcnv3 = tConv2d(tcnv2 + cnv2, [3, 3], 8, strides = [2, 2], name = 'tcnv3') # --> 48 x 48 x 8
+        tcnv4 = tConv2d(tcnv3 + cnv1, [3, 3], 3, strides = [2, 2], name = 'tcnv4') # --> 96 x 96 x 3
         #Some fancy stuff
-        self.cnv5 = conv2d(self.tcnv4 + x, [3, 3], 16) # --> 96 x 96 x 16
-        self.res1 = res_block(self.cnv5, [4, 4], 32) # --> 96 x 96 x 32
-        self.logit = conv2d(self.res1, [4, 4], 3) # --> 96 x 96 x 3
-
+        cnv5 = conv2d(tcnv4 + x, [3, 3], 16, name = 'cnv5') # --> 96 x 96 x 16
+        res1 = res_block(cnv5, [4, 4], 32, name = 'res1') # --> 96 x 96 x 32
+        logit = conv2d(res1, [4, 4], 3, name = 'logit') # --> 96 x 96 x 3
         #Output 
-        self.out = tf.nn.sigmoid(self.logit)
+        return logit
 
-class Disc():
-
-    def __init__(self, x):
+def disc(x, name = 'disc'):
+    with tf.variable_scope(name):
         #96 x 96 x 3
-        self.cnv1 = conv2d(x, [3, 3], 16, strides = [2, 2]) # --> 48 x 48 x 8
-        self.cnv2 = conv2d(self.cnv1, [3, 3], 32, strides = [2, 2]) # --> 24 x 24 x 32
-        self.cnv3 = conv2d(self.cnv2, [3, 3], 64, strides = [2, 2]) # --> 12 x 12 x 64
-        self.cnv4 = conv2d(self.cnv3, [3, 3], 128, strides = [2, 2]) # --> 6 x 6 x 128 
-        self.cnv5 = conv2d(self.cnv4, [3, 3], 64, strides = [2, 2]) # --> 3 x 3 x 64 
-        self.cnv7 = conv2d(self.cnv6, [3, 3], 1, strides = [1, 1], padding = 'VALID') # --> 1 x 1 x 1
+        cnv1 = conv2d(x, [3, 3], 16, strides = [2, 2], name = 'cnv1') # --> 48 x 48 x 8
+        cnv2 = conv2d(cnv1, [3, 3], 32, strides = [2, 2], name = 'cnv2') # --> 24 x 24 x 32
+        cnv3 = conv2d(cnv2, [3, 3], 64, strides = [2, 2], name = 'cnv3') # --> 12 x 12 x 64
+        cnv4 = conv2d(cnv3, [3, 3], 128, strides = [2, 2], name = 'cnv4') # --> 6 x 6 x 128 
+        cnv5 = conv2d(cnv4, [3, 3], 64, strides = [2, 2], name = 'cnv5') # --> 3 x 3 x 64 
+        logit = conv2d(cnv5, [3, 3], 1, strides = [1, 1], padding = 'valid', name = 'cnv6') # --> 1 x 1 x 1
+        return logit
---
+++ train.py
@@ -0,0 +1,80 @@
+import tensorflow as tf 
+import numpy as np 
+from matplotlib import pyplot as plt 
+import skimage 
+import os
+
+import nets
+
+path = './Datasets/processed/'
+dom_a = 'celeb/'
+dom_b = 'anime/'
+
+a_namelist = os.listdir(path + dom_a)
+b_namelist = os.listdir(path + dom_b)
+
+#Loading data 
+class datGen():
+    
+    def make_batch(self, dat, path, batch_size, ind = 0, offset = 0,  aug_chance = 0.3):
+        imgs = []
+        rnd_indexes = np.array(range(batch_size)) + ind * batch_size + offset
+        for index in rnd_indexes:
+            imgs.append(self.fetch_img('%s/%s' % (path, dat.loc[index][0]), aug_chance))
+        # returns normalized and flattened images along with the labels
+        return np.reshape(np.array(imgs) / 256, [batch_size, 96 * 96 * 4])
+
+    def fetch_img(self, path, aug_chance):
+        r = np.random.uniform()
+        angle = 0
+        if r < aug_chance:
+            angle = np.random.uniform(90)
+        return self.augment(skimage.io.imread('%s' % (path)), angle = angle)
+    
+    def augment(self, img, angle = 0):
+        if angle == 0:
+            return img
+        return skimage.transform.rotate(img, angle)
+
+#Define ops 
+a_real = tf.placeholder(tf.float32, shape = [None, 96, 96, 3])
+b_real = tf.placeholder(tf.float32, shape = [None, 96, 96, 3])
+
+with tf.variable_scope('Model') as scope:
+    #Generator output
+    b_gen = nets.gen(a_real, name = 'g_AtoB')
+    a_gen = nets.gen(b_real, name = 'g_BtoA')
+
+    #Discriminator outputs for real inputs
+    a_dis = nets.disc(a_real, name = 'disc_A')
+    b_dis = nets.disc(b_real, name = 'disc_B')
+
+    #Discriminator outputs for fake input 
+    a_gen_dis = nets.disc(a_gen, name = 'disc_A')
+    b_gen_dis = nets.disc(b_gen, name = 'disc_B')
+
+    #Cyclic generator output
+    a_cyc = nets.gen(b_gen, name = 'g_BtoA')
+    b_cyc = nets.gen(a_gen, name = 'g_AtoB')
+
+#Discriminator loss for real inputs 
+loss_dis_a = tf.reduce_mean(tf.squared_difference(a_dis, 1))
+loss_dis_b = tf.reduce_mean(tf.squared_difference(b_dis, 1))
+
+#Discriminator loss for fake inputs
+loss_dis_a = tf.reduce_mean(tf.squared_difference(a_gen_dis, 0))
+loss_dis_b = tf.reduce_mean(tf.squared_difference(b_gen_dis, 0))
+
+#Generator loss
+loss_gen_a_1 = tf.reduce_mean(tf.squared_difference(a_gen_dis, 1))
+loss_gen_b_1 = tf.reduce_mean(tf.squared_difference(b_gen_dis, 1))
+
+#Cyclic loss 
+loss_cyc = tf.reduce_mean(tf.squared_difference(a_real, a_cyc)) + tf.reduce_mean(tf.squared_difference(b_real, b_cyc))
+
+#Combined loss 
+loss_gen_a = loss_gen_a_1 + 10 * loss_cyc
+loss_gen_b = loss_gen_b_1 + 10 * loss_cyc
+
+tvars = tf.trainable_variables(scope = 'Model')
+
